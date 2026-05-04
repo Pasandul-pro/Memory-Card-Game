ea098c4e3dfee78b687d7a1ff145fb38fbc17e61
Pasandul Wickramaarachchi <thevinuomisha@gmail.com>
2026-05-04 22:40:58 +0530

    feat: implement base structure and styling for memory card game components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { GameHeader } from './Components/GameHeader'
+import { Card } from './Components/Card'
+
+const cardValues = [
+  "🍎",
+  "🍌",
+  "🍇",
+  "🍊",
+  "🍓",
+  "🥝",
+  "🍑",
+  "🍒",
+  "🍎",
+  "🍌",
+  "🍇",
+  "🍊",
+  "🍓",
+  "🥝",
+  "🍑",
+  "🍒",
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    </>
+    <div className='app'>
+      <GameHeader score={0} moves={1}/>
+      <div className="cards-grid">
+        {cardValues.map((card) => (
+          <Card card={card}/>
+        ))}
+      </div>
+    </div>
   )
 }
 
